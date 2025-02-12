1d5bd371ba68a3d7a87afde0ef747cbdc59d8743
datamonk <acamara86@gmail.com>
2025-02-12 00:55:39 -0500

    save for init.d template

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 function defs(){
+  wai=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd);
+  source $wai/.lib/src-libs.sh
+  source $wai/.env
+
   __PROGNAME="${0##*/}"
   __die(){ echo "${__PROGNAME}: error: $1" 1>&2; exit 1; };
   __isnotempty(){ [ "$1" ] && return 0 || return 1; };
@@ -10,21 +14,16 @@ function defs(){
   #    __validate_input "${FUNCNAME[0]}" "$1"
   #    __isnotempty "$(container_id "$1")"
   };
-
 };
 
-if [[ $(id -u) -ne  0 ]]; then
-  log_error "Bootstrap init needs root exec. uid = $(id -u)"
-  exit 1
-fi
 function mntUSB(){
   local -r usbdev='/dev/sda1';
-  local -r usbmnt='/mnt/usb';
-  local -r mntuser='afcamar';
+  local -r usbmnt="/media/$USER/ORBITKEY";
+  local -r mntuser="$USER";
   if [[ -b "$usbdev" ]]; then
-     sudo mount $usbdev $usbmnt -o uid=$mntuser,gid=$mntuser \
-    && bsdir="$usbmnt/hc/bootstrap"
-    log_info "USB media mounted at: [$(mount | grep $usbmnt)]"
+  # sudo mount $usbdev $usbmnt -o uid=$mntuser,gid=$mntuser \
+    bsdir="$usbmnt/hc/bootstrap"
+    #log_info "USB media mounted at: [$(mount | grep $usbmnt)]"
   fi
 };
 function mkDirs(){
@@ -41,8 +40,8 @@ function mkDirs(){
   && ln -s .secrets .creds
 };
 function confGit(){
-  local -r gitemail='acamara86@gmail.com';
-  local -r gituser='Andy Camara';
+  local -r gitemail="";
+  local -r gituser="$USER";
   local -r gitcreds="$bsdir/.git-token";
   local -r bsrepo="https://$(cat $gitcreds)@github.com/datamonk/hamster-cannon.git";
   if [[ -f "$gitcreds" ]]; then
@@ -55,11 +54,10 @@ function confGit(){
 };
 function confSsh(){
   local -r newkeypair="true"; # false to cp from usb
-  mkd(){
-    mkdir -p "$@" && chmod 740 "$_" && cd "$_";
-  };
+  mkdir -p "~/.ssh" && chmod 740 "~/.ssh" && cd "~/.ssh";
+
   sudo sed -i.bak s/\#PasswordAuthentication\ yes/PasswordAuthentication\ yes/ /etc/ssh/sshd_config \
-  &&  sudo systemctl start ssh
+  &&  sudo systemctl enable ssh && sudo systemctl start ssh
   if [[ "$newkeypair" = "true" ]]; then
     #ssh-keygen -t rsa && ssh-add
     ssh-keygen -b 4096 -q -t rsa -P '' -f id_rsa && ssh-add
@@ -67,21 +65,21 @@ function confSsh(){
     # set source perms to 600 for files
     cp --preserve=mode,ownership,timestamps --no-clobber "$bsdir/.ssh/*" "$HOME/.ssh/" \
     && ssh-add
-    install -m 600 /dev/null ${SSH_DIR}/config
-    echo 'ForwardX11Trusted yes' >> ${SSH_DIR}/config
-    echo 'ConnectTimeout 0' >> ${SSH_DIR}/config
-    echo '' >> ${SSH_DIR}/config
-    echo 'Host localhost' >> ${SSH_DIR}/config
-    echo "  HostName $(hostname)" >> ${SSH_DIR}/config # note prefixed [spaces]
-    echo '' >> ${SSH_DIR}/config
-    echo 'Host *' >> ${SSH_DIR}/config
-    echo '  ForwardX11 yes' >> ${SSH_DIR}/config
-    echo '  ForwardAgent yes' >> ${SSH_DIR}/config
   fi
+  install -m 600 /dev/null ${SSH_DIR}/config
+  echo 'ForwardX11Trusted yes' >> ${SSH_DIR}/config
+  echo 'ConnectTimeout 0' >> ${SSH_DIR}/config
+  echo '' >> ${SSH_DIR}/config
+  echo 'Host localhost' >> ${SSH_DIR}/config
+  echo "  HostName $(hostname)" >> ${SSH_DIR}/config # note prefixed [spaces]
+  echo '' >> ${SSH_DIR}/config
+  echo 'Host *' >> ${SSH_DIR}/config
+  echo '  ForwardX11 yes' >> ${SSH_DIR}/config
+  echo '  ForwardAgent yes' >> ${SSH_DIR}/config
 };
 function updatePi(){
   sudo apt --yes update \
-  &&  sudo apt --yes full-upgrade \
+  &&  sudo apt --yes upgrade \
   &&  sudo apt --yes install \
       htop \
       vim \
@@ -89,11 +87,32 @@ function updatePi(){
       nodejs \
       default-jdk \
       nodejs \
+      code \
   && sudo apt --yes autoremove \
   && sudo rpi-eeprom-update -a
 };
+function installDocker(){
+  sudo ${WAI}/install-docker.sh --channel stable # --include-dryrun not working...
+  if [[ $(docker_is_active) = 0 ]]; then
+    sudo usermod -aG docker $USER
+    if [[ $(is_docker_grp_member $USER) = 0 ]]; then
+      log_info "$USER has successfully been added to docker group."
+      log_warn "rebooting in 15 seconds..."
+      sleep 5
+      log_warn "rebooting in 10 seconds..."
+      sleep 5
+      log_warn "rebooting in 5 seconds..."
+      sleep 5 && shutdown -r now
+    fi
+  else
+    log_error "docker service is not running after installation."
+    log_error "this issue needs to be addressed before proceeding."
+    log_error "#######"
+    sudo systemctl status docker && sudo systemctl status docker.socket
+    log_error "#######"
+  fi
+};
 function bounce(){
-  echo "bootstrap-init stage done."
   echo "rebooting in 15 seconds..." && sleep 5
   echo "rebooting in 10 seconds..." && sleep 5
   echo "rebooting in 5 seconds..." && sleep 5
