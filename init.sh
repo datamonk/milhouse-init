749e7cf9484a46d654e5e67c145a18e450588791
datamonk <acamara86@gmail.com>
2025-02-12 20:53:03 -0500

    test 2 corrections

--- init.sh
+++ init.sh
@@ -1,60 +1,70 @@
 #!/bin/bash
 
-function defs(){
-  wai=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd);
-  source $wai/.lib/src-libs.sh
-  source $wai/.env
-
-  __PROGNAME="${0##*/}"
-  __die(){ echo "${__PROGNAME}: error: $1" 1>&2; exit 1; };
-  __isnotempty(){ [ "$1" ] && return 0 || return 1; };
-  __validate_input(){
-     [ "$2" ] || __die "$1 requires an argument (container name)"
-  #  example usage:
-  #    __validate_input "${FUNCNAME[0]}" "$1"
-  #    __isnotempty "$(container_id "$1")"
-  };
+__PROGNAME="${0##*/}"
+__die(){ echo "${__PROGNAME}: error: $1" 1>&2; exit 1; };
+__isnotempty(){ [ "$1" ] && return 0 || return 1; };
+__validate_input(){
+   [ "$2" ] || __die "$1 requires an argument str"
+#  example usage:
+#    __validate_input "${FUNCNAME[0]}" "$1"
+#    __isnotempty "$(container_id "$1")"
 };
+mkd(){ mkdir -p "$@" && cd "$_"; };
+
+wai=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd);
+#source $wai/.lib/src-libs.sh
+source $wai/.env
 
 function mntUSB(){
   local -r usbdev='/dev/sda1';
-  local -r usbmnt="/media/$USER/ORBITKEY";
-  local -r mntuser="$USER";
   if [[ -b "$usbdev" ]]; then
-    sudo mount $usbdev $usbmnt -o uid=$mntuser,gid=$mntuser \
-    bsdir="$usbmnt/hc/bootstrap"
-    log_info "USB media mounted at: [$(mount | grep $usbmnt)]"
+    usbmnt=$(mount | grep "$usbdev" | cut -d' ' -f3)
+    if [[ $(__isnotempty $usbmnt) -eq 0 ]]; then
+      log_info "existing $usbdev mnt found at [ $usbmnt ]"
+      continue
+    else
+      usbmnt="/media/$USER/ORBITKEY"; # failback to static def
+      log_warn "$usbdev mnt point returned null. attempting to mnt at [ $usbmnt ]"
+      sudo mount $usbdev $usbmnt -o uid=$USER,gid=$USER \
+      && log_info "USB media mounted at: [$(mount | grep $usbmnt)]"
+    fi
+    bsdir="$usbmnt/hc/bootstrap";
+  else
+    log_error "usb dev path not found."
+    __die "${FUNCNAME[0]}: unable to mnt usb for secrets def. bailing."
   fi
 };
 function mkDirs(){
-  mkdir -p "$HOME/proj/github" \
-           "$HOME/proj/software" \
+  cd $HOME
+  mkdir -p "$HOME/proj/software" \
            "$HOME/data" \
            "$HOME/certs" \
            "$HOME/scripts" \
            "$HOME/tmp" \
            "$HOME/.secrets"
-  cd $HOME \
-  && ln -s proj/github gh \
+  ln -s proj/github gh \
   && ln -s proj/software sw \
   && ln -s .secrets .creds
 };
 function confGit(){
-  local -r gitemail="";
-  #local -r gituser="$USER";
+  local -r gitemail="acamara86@gmail.com";
   local -r gitcreds="$bsdir/.git-token";
   local -r bsrepo="https://$(cat $gitcreds)@github.com/datamonk/hamster-cannon.git";
   if [[ -f "$gitcreds" ]]; then
     log_info "Applying global setting for git env."
     git config --global user.email "$gitemail" \
     && git config --global user.name "$USER" \
-    && git config --global credential.helper "store --file $HOME/.git-credentials" \
+    && git config --global credential.helper "store --file $HOME/.git-credentials"
+    mkd "$HOME/proj/github" \
     && git clone "$bsrepo"
+    if [[ ! -f ./"README.md" ]]; then
+      log_error "hamster-cannon repo clone failed."
+    fi
   fi
 };
 function confSsh(){
   local -r newkeypair="true"; # false to cp from usb
-  mkdir -p "~/.ssh" && chmod 740 "~/.ssh" && cd "~/.ssh";
+  mkd "$HOME/.ssh" && chmod 740 "$wai/../.ssh"
 
   sudo sed -i.bak s/\#PasswordAuthentication\ yes/PasswordAuthentication\ yes/ /etc/ssh/sshd_config \
   &&  sudo systemctl enable ssh && sudo systemctl start ssh
@@ -63,10 +73,9 @@ function confSsh(){
     ssh-keygen -b 4096 -q -t rsa -P '' -f id_rsa && ssh-add
   else
     # set source perms to 600 for files
-    cp -R --preserve=mode,ownership,timestamps --no-clobber "$bsdir/.ssh/*" "$HOME/.ssh/" \
-    && ssh-add
+    cp -R --preserve=mode,timestamps --no-clobber "$bsdir/.ssh/id_*" "$HOME/.ssh/" && ssh-add
   fi
-  install -m 600 /dev/null $sshdir/config
+  install -m 600 /dev/null $HOME/.ssh/config
   echo -e \
     "ForwardX11Trusted yes\n
     ConnectTimeout 0\n\n
@@ -74,7 +83,7 @@ function confSsh(){
     \s\sHostName $(hostname)\n\n
     Host *\n
     \s\sForwardX11 yes\n
-    \s\sForwardAgent yes" >> $sshdir/config
+    \s\sForwardAgent yes" >> $HOME/.ssh/config
 };
 function updatePi(){
   sudo apt --yes update \
@@ -91,34 +100,32 @@ function updatePi(){
   && sudo rpi-eeprom-update -a
 };
 function installDocker(){
-  sudo ${wai}/install-docker.sh --channel stable
-  if [[ $(docker_is_active) = 0 ]]; then
+  sudo $wai/install-docker.sh --channel stable
+  if [[ $(sudo systemctl is-active --quiet docker) -eq 0 ]]; then
     sudo usermod -aG docker $USER
-    if [[ $(is_docker_grp_member $USER) = 0 ]]; then
-      log_info "$USER has successfully been added to docker group."
+    if [[ $(sudo getent group docker | grep $USER) -eq 0 ]]; then
+      local -r usergrps=$(id -g -n)
+      log_info "$USER added to 'docker' grp.\n [ $usergrps ]"
+      log_info "stage 00 complete for $HOSTNAME bootstrap."
+      log_warn "rebooting in 15 seconds..." && sleep 5
+      log_warn "rebooting in 10 seconds..." && sleep 5
+      log_warn "rebooting in 5 seconds..." && sleep 5
+      shutdown -r now
     fi
   else
-    log_error "docker service is not running after installation."
-    log_error "this issue needs to be addressed before proceeding."
-    log_error "#######"
-    sudo systemctl status docker && sudo systemctl status docker.socket
-    log_error "#######"
-    exit 1
+    log_error "docker service is not running after install."
+    __die "${FUNCNAME[0]}: docker is not happy. bailing."
   fi
 };
-function bounce(){
-  log_warn "rebooting in 15 seconds..." && sleep 5
-  log_warn "rebooting in 10 seconds..." && sleep 5
-  log_warn "rebooting in 5 seconds..." && sleep 5
-  shutdown -r now
-};
+
 function fire(){
-  #mntUSB
+  mntUSB
   mkDirs
   confGit
   confSsh
   updatePi
   bounce
 };
+
 fire
 exit 0
